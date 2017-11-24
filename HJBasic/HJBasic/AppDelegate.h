ee97d6f6c24c103c975a4402dc089a1dce00da86
夏航军 <xiahangjun@9188.com>
2017-11-24 11:19:42 +0800

    新建项目

---
+++ HJBasic/HJBasic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HJBasic
+//
+//  Created by a-375 on 2017/11/24.
+//  Copyright © 2017年 a-375. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HJBasic/HJBasic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HJBasic
+//
+//  Created by a-375 on 2017/11/24.
+//  Copyright © 2017年 a-375. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
